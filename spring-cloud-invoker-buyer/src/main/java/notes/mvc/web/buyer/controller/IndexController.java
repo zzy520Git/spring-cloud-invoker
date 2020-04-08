992ae2fb8a75438089d1ae872474f018182158e7
zhouzhongyi <zzy520com@qq.com>
2020-04-08 23:27:16 +0800

    xml-json

--- spring-cloud-invoker-buyer/src/main/java/notes/mvc/web/buyer/controller/IndexController.java
+++ spring-cloud-invoker-buyer/src/main/java/notes/mvc/web/buyer/controller/IndexController.java
@@ -1,11 +1,14 @@
 package notes.mvc.web.buyer.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import notes.common.util.Result;
 import notes.mvc.web.buyer.service.FeignTestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 /**
  * Description：
@@ -36,5 +39,20 @@ public class IndexController {
     public String useFeign() {
         log.error("feign测试");
         return feignTestClient.invokeProvider();
+    }
+
+    @RequestMapping("/result")
+    public Result getResult() {
+
+        Result forObject = restTemplate.getForObject("http://eureka-provider/provider/obj", Result.class);
+        try {
+            Map data = (Map) forObject.getData();
+            if (data != null) {
+                log.warn("商家=【name={}】-【age={}】", data.get("name"), data.get("age"));
+            }
+        } catch (Exception e) {
+            log.error("强制类型转换异常={}", forObject.toString(), e);
+        }
+        return forObject;
     }
 }
